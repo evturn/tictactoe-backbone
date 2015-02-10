9630e19eea47acfb9cbb4fd7ee3b9ebb11b39e21
Evan Turner <evturn@gmail.com>
2015-02-10 17:12:44 -0500

    game knows when i slot is already taken

--- js/views/cellView.js
+++ js/views/cellView.js
@@ -1,18 +1,27 @@
 var CellView = Backbone.View.extend({
 	template: _.template($('#cell-template').html()),
 	initialize: function() {
+		gamePlay = [];
 		this.render();
 	},
 	events: {
-		'click .cell': 'replaceCell'
+		'click .cell': 'isUsed'
 	},
 	render: function() {
 		this.$el.html(this.template(this.model.toJSON()));
 		return this;
 	},
-	replaceCell: function(e) {
+	isUsed: function(e) {
 		e.preventDefault();
-		cellId = this.model.get('cell');
+		usedCell = this.model.get('cell');
+		if ($.inArray(usedCell, gamePlay) !== -1) {
+			alert('It\s taken. You blind?');
+		} else {
+			gamePlay.push(usedCell);
+			this.replaceCell();	
+		}
+	},
+	replaceCell: function() {
 		allDonuts = donutsCollection.models;
 		newDonut = allDonuts[Math.floor(Math.random()*allDonuts.length)];
 		newDonut.set({cell: cellId});
